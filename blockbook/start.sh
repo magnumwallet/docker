c0a4dbfc3e1d87f510366a3a87a840c5b171909a
smdmitry <smdmitry@gmail.com>
2019-03-29 17:39:13 +0300

    myriad

--- blockbook/start.sh
+++ blockbook/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ./blockbook -sync -datadir=./data -blockchaincfg=./blockchaincfg.json -log_dir=./logs -dbcache=1073741824 -explorer=https://btc.mgnm.rocks -internal=:9030 -public=:9130 -logtostderr -debug
+
+./blockbook -sync -datadir=./data -blockchaincfg=./myriad.json -log_dir=./logs -dbcache=1073741824 -explorer=https://xmy.mgnm.rocks -internal=:4021 -public=:2021 -logtostderr -debug
