7538d4ca1e8c0699dac50b916cb0e1c0394b16f0
smdmitry <smdmitry@gmail.com>
2019-09-23 23:56:42 +0300

    beldex

--- node-beldex/data/start.sh
+++ node-beldex/data/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /root/beldex/build/release/bin/beldexd --detach
-/root/beldex-web-wallet/build/beldex-web-wallet -p 1985 -c /root/.beldex/openmonero.json
+/root/beldex-web-wallet/build/beldex-web-wallet -p 1985 -c /root/.beldex/beldex.json
