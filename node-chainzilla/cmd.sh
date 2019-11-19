d1ea0ab60cb989699af1cc28dde979861920e716
smdmitry <smdmitry@gmail.com>
2019-11-19 19:11:11 +0300

    chainzilla

--- node-chainzilla/cmd.sh
+++ node-chainzilla/cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/komodo-explorer/node_modules/bitcore-node-komodo/bin/bitcore-node start
+/opt/node/bin/node -datadir=/mnt/data -ac_name=ZILLA -ac_supply=11000000 -ac_sapling=5000000 -addnode=51.68.215.104
